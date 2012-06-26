ca63789ba611075e42cdfb867fc9e58e58714cc5
fls <lisheng.fu@csst.com>
2012-06-26 13:44:39 +0800

    add commit tomcat-deploy script

---
+++ module-jdk/manifests/init.pp
@@ -0,0 +1,17 @@
+# Class: module-jdk
+#
+# This module manages module-jdk
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class java {
+     include java::params,java::install
+
+}
