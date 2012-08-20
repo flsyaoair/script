6ab6fe246cf1c06576cb5f709348e87eea2c7269
fls <lisheng.fu@csst.com>
2012-08-20 14:30:25 +0800

    add gitweb install-no test

--- manifests/gitosis.pp
+++ manifests/gitosis.pp
@@ -6,8 +6,17 @@
 #  - Class[git]
 #
 class git::gitosis {
+	$projectroot="/home/git/repositories/"
 #  include ::git
-  package {'gitosis':
-    ensure => present
+  package {['gitosis',"gitweb"]:
+    ensure => present,
+    alias =>"gitinstall",
+  }
+  file {
+  	"/etc/gitweb.conf":
+  	ensure => present,
+  	recurse => true,
+  	content =>template("gitweb.conf.erb"),
+  	require => Package ["gitinstall"]
   }
 }
