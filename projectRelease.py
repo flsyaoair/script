7b9b68739346d8fe4226c09ba31887387b09d675
fls <Administrator@flsadministrator>
2013-06-28 15:22:47 +0800

    add

--- projectRelease.py
+++ projectRelease.py
@@ -20,14 +20,14 @@ versionFile=open('version.txt','r')
 content=versionFile.readlines() 
 versionFile.close() 
 path=r'Z:\nexus-2.0.4-1-bundle\sonatype-work\nexus\storage\releases'
-print content[1].strip('\n')
+#print content[1].strip('\n')
 path=path + content[0].strip('\n')
-print path
+#print path
 os.system ("echo %s >>%s" %('%date%','release.txt'))
 for line in content[1:]:
      lineContent=line.strip('\n')
      os.system ('mvn -P %s' %(lineContent))
-     os.system ("echo %s %s >>%s" %(lineContent,'release.txt'))
+     os.system ("echo %s >>%s" %(lineContent,'release.txt'))
      os.system ("cp -r %s %s" %('release.txt', path))     
      print  "%s is there %s" %(line,'area !')
 
