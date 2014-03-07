4b9cc48bbf02700fa38a51556534951b98eafcdf
fls@csst.com <fls@csst.com>
2014-03-07 17:52:31 +0800

    test

---
+++ nagios.py
@@ -0,0 +1,14 @@
+import sys
+values=[1,2,3]
+def test():
+    for i in values:
+#        print i
+        if i==6:
+            print 'ok'
+#        else:
+#            sys.exit(1)
+    return sys.exit(1)
+run=test()
+print run
+            
+    
